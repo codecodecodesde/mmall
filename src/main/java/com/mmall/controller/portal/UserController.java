d365f29b4f015310db1c13b979634211eb5ff498
misspink1011 <xuanwang1118@gmail.com>
2018-04-11 18:35:08 -0400

    user register, logout and validate

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -6,6 +6,7 @@ import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -38,4 +39,24 @@ public class UserController {
         }
         return response;
     }
+
+    @RequestMapping(value = "logout.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session) {
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.createBySuccess();
+    }
+
+    @RequestMapping(value = "register.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> register(User user) {
+        return iUserService.register(user);
+    }
+
+    @RequestMapping(value = "check_valid.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> checkValid(String str, String type) {
+        return iUserService.checkValid(str, type);
+    }
+
 }
--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mmall.service.impl;
 
+import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
 import com.mmall.dao.UserMapper;
 import com.mmall.pojo.User;
@@ -7,6 +8,7 @@ import com.mmall.service.IUserService;
 import com.mmall.util.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 /**
  * Created by erin.
@@ -33,4 +35,50 @@ public class UserServiceImpl implements IUserService{
         user.setPassword(org.apache.commons.lang3.StringUtils.EMPTY);
         return ServerResponse.createBySuccess("Login successfully", user);
     }
+
+    @Override
+    public ServerResponse<String> register(User user) {
+
+        ServerResponse validResponse = this.checkValid(user.getUsername(), Const.USERNAME);
+        if(!validResponse.isSuccess()) {
+            return validResponse;
+        }
+
+        validResponse = this.checkValid(user.getEmail(), Const.EMAIL);
+        if(!validResponse.isSuccess()) {
+            return validResponse;
+        }
+        user.setRole(Const.Role.ROLE_CUSTOMER);
+        user.setPassword(MD5Util.MD5EncodeUtf8(user.getPassword()));
+
+        int resultCount = userMapper.insert(user);//#lines inserted into DB
+        if(resultCount == 0) {
+            return ServerResponse.createByErrorMessage("Register failed");
+        }
+        return ServerResponse.createBySuccessMessage("Register succeeded.");
+
+    }
+
+    public ServerResponse<String> checkValid(String str, String type) {
+        if(org.apache.commons.lang3.StringUtils.isNoneBlank(type)) {
+            if(Const.USERNAME.equals(type)){
+                int resultCount = userMapper.checkUsername(str);
+                if(resultCount > 0 ){
+                    return ServerResponse.createByErrorMessage("Username has already existed.");
+                }
+            }
+
+            if(Const.EMAIL.equals(type)) {
+                int resultCount = userMapper.checkEmail(str);
+                if (resultCount > 0) {
+                    return ServerResponse.createByErrorMessage("Email has already existed.");
+                }
+            }
+
+            return ServerResponse.createBySuccessMessage("Validation succeeded");
+        }else {
+            return ServerResponse.createByErrorMessage("Wong parameters");
+        }
+    }
+
 }
